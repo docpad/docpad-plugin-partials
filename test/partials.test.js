5b30c989179f27f5873b967beb0873d616e53632
Benjamin Lupton <b@lupton.cc>
2012-06-11 20:07:38 +1000

    Published v2.0.0 of every plugin for DocPad v6.0.0

--- test/partials.test.js
+++ test/partials.test.js
@@ -1,2 +1,2 @@
 // Test our plugin using DocPad's Testers
-require('docpad/lib/testers').test({pluginPath: __dirname+'/..'})
+require('docpad').require('testers').test({pluginPath: __dirname+'/..'});
